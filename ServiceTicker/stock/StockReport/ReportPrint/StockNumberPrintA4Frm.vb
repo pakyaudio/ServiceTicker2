f3d11c529483859dc26644a3e841950c1657733b
pakyaudio <pakyaudio@gmail.com>
2017-04-18 19:57:35 +0700

    Add files via upload

---
+++ ServiceTicker/stock/StockReport/ReportPrint/StockNumberPrintA4Frm.vb
@@ -0,0 +1,12 @@
+﻿Public Class StockNumberPrintA4Frm
+
+    Private Sub StockNumberPrintA4Frm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Icon = MainFrm.Icon
+        'TODO: This line of code loads data into the 'MySQLReport.company' table. You can move, or remove it, as needed.
+        Me.companyTableAdapter.Fill(Me.MySQLReport.company)
+        'TODO: This line of code loads data into the 'STdata.StockAllReport' table. You can move, or remove it, as needed.
+        Me.StockAllReportTableAdapter.Fill(Me.STdata.StockAllReport)
+        Me.ReportViewer1.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout)
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
