ee0c71d87fcce60fbe1bd764bba0a3d0985e5b66
pakyaudio <pakyaudio@gmail.com>
2017-04-18 17:42:42 +0700

    Add files via upload

---
+++ ServiceTicker/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ServiceTicker")> 
+<Assembly: AssemblyDescription("โปรแกรมนี้จัดทำและแจกจ่ายใช้งานฟรี โดยมีเงื่อนไขห้ามนำไปจำหน่ายโดยคิดมูลค่าของโปรแกรม โปรแกรมนี้ถูกคุ้มครองลิขสิทธิ์โดยอัตโนมัติ ลิขลิทธิ์เฉพาะ ของ พี เอ เทคกรุ๊ป เท่านั้น โปรแกรมมีปัญหา แจ้งที่ pakyaudio@gmail.com หรือ https://www.facebook.com/pakyaudio/")> 
+<Assembly: AssemblyCompany("P A Tech Group")> 
+<Assembly: AssemblyProduct("ServiceTicker")> 
+<Assembly: AssemblyCopyright("ฟรีซอฟท์แวร์ 2014 - ปัจจุบัน")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("82319f5e-05c9-4844-8125-b7d845cfe6bc")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2.0.0.34")> 
+<Assembly: AssemblyFileVersion("2.0.0.34")> 
